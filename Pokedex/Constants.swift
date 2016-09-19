517fd46433d50c5b6c0d10da03340f16d0e530cc
Cody Condon <codycondon50@gmail.com>
2016-09-19 18:07:46 -0400

    init Cocoapods and add alamofire

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Cody Condon on 2016-09-19.
+//  Copyright © 2016 Cody Condon. All rights reserved.
+//
+
+import Foundation
+
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,6 +11,20 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     @IBOutlet weak var nameLbl: UILabel!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIDLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
     var pokemon: Pokemon! 
 
     override func viewDidLoad() {
@@ -19,6 +33,8 @@ class PokemonDetailVC: UIViewController {
       
     }
 
-  
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
